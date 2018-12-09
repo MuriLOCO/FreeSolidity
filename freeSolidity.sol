531a12d0d93cefc3edbbf05ccce0349e4c7c1ead
yeez <y.m.alhasan@gmail.com>
2018-12-09 13:54:00 -0500

    refactoring

--- freeSolidity.sol
+++ freeSolidity.sol
@@ -1,53 +1,123 @@
-pragma solidity ^0.4.25;
-contract availableService{
+pragma solidity ^0.5.1;
+
+contract Service{
+       
+    uint serviceId;
+    string serviceName;
+    string serviceDescription;
     
-    uint counter;
-    address contractOnwerAddress;
-    
-    struct Service{
-        uint serviceId;
-        string serviceName;
-        string serviceDescription;
-    }
-        
-    mapping (uint => Service) services;
-   
-    constructor() public {
-        contractOnwerAddress = msg.sender;
-        counter = 0;
-    }
-   
-    function createService(string memory _serviceName, string memory _serviceDescription) public {
-        require(contractOnwerAddress == msg.sender,"only the owner can create a service");
-        uint serviceId = counter++;
-        services[serviceId] = Service(serviceId, _serviceName, _serviceDescription);
-    }
-   
-    function getServiceById(uint _serviceId) public view returns (string memory _serviceName){
-        return services[_serviceId].serviceName;
-    }
+   constructor (uint id, string memory _serviceName, string  memory _serviceDescription) public{
+       serviceId = id;
+       serviceName = _serviceName;
+       serviceDescription = _serviceDescription;
+   }
 }
 
-contract availableServiceProvider is availableService{
+contract Client{
     
-    struct ServiceProvider{
-        uint servicerProviderId;
-        string serviceProviderName;
-        string serviceProviderPhoneNumber;
-        string serviceProviderEmail;
-        address serviceProviderAddress;
-        uint rank;
-        Service service;
+    uint clientId;
+    string clientName;
+    string clientPhoneNumber;
+    string clientEmail;
+    
+    constructor (uint id, string memory clientNameMeta, string  memory cientPhoneNumberMeta, 
+    string memory cientEmailMeta) public{
+       clientId = id;
+       clientName = clientNameMeta;
+       clientPhoneNumber = cientPhoneNumberMeta;
+       clientEmail = cientEmailMeta;
+   }
+}
+
+contract ServiceProvider{
+    
+    uint servicerProviderId;
+    string serviceProviderName;
+    string serviceProviderPhoneNumber;
+    string serviceProviderEmail;
+    address serviceProviderAddress;
+    uint rank;
+    Service serviceProvided; //has a service
+    
+    constructor (uint id, string memory serviceProviderNameMeta, string  memory serviceProviderPhoneNumberMeta, 
+    string memory serviceProviderEmailMeta,  address serviceProviderAddressMeta, uint rankMeta) public{
+       servicerProviderId = id;
+       serviceProviderName = serviceProviderNameMeta;
+       serviceProviderPhoneNumber = serviceProviderPhoneNumberMeta;
+       serviceProviderEmail = serviceProviderEmailMeta;
+       serviceProviderAddress = serviceProviderAddressMeta;
+       rank = rankMeta;
+   }
+   
+   function assignServiceProvided (Service serviceProvidedMeta) public{
+       serviceProvided = serviceProvidedMeta;
+   }
+}
+    
+    
+contract FreeSolidityApplication{
+    
+    // struct Service{
+    //     uint serviceId;
+    //     string serviceName;
+    //     string serviceDescription;
+    //     }
+    
+    uint serviceIdCounter;
+    uint serviceProviderIdCounter;
+    uint clientIdCounter;
+    address contractOwnerAddress;
+    
+    mapping (uint => Service) serviceMap;
+    mapping (uint => ServiceProvider) serviceProviderMap;
+    mapping (uint => Client) clientMap;
+    
+    // ServiceProvider registration fee payment
+    modifier payServiceProviderRegistrationFee(){
+        require (msg.value >= 2000000000000000000);
+        contractOwnerAddress.transfer(msg.value);
+        _;
     }
+   
+   constructor() public{
+       contractOwnerAddress = msg.sender;
+       serviceIdCounter = 0;
+       serviceProviderIdCounter = 0;
+       clientIdCounter = 0;
+   }
+   
+   function addNewSupportedService(string memory _serviceName, string memory _serviceDescription) public {
+       require(contractOwnerAddress == msg.sender);
+       uint serviceId = serviceIdCounter++;
+       Service newService = new Service(serviceId, _serviceName, _serviceDescription);
+       serviceMap[serviceId] = newService;
+   }
+   
+   function registerAsServiceProvider(string memory _serviceProviderName, 
+   string memory _serviceProviderPhoneNumber, string memory _serviceProviderEmail, address _serviceProviderAddress,
+   uint _rank) public payServiceProviderRegistrationFee {
+       
+       uint serviceProviderId = serviceProviderIdCounter++;
+       ServiceProvider newServiceProvider = ServiceProvider (serviceProviderId, _serviceProviderName, _serviceProviderPhoneNumber,
+       _serviceProviderEmail, _serviceProviderAddress);
+       serviceProviderMap[serviceProviderId] = newServiceProvider;
+   }
+   
+  function getServiceById(uint _serviceId) public returns (string memory _serviceName){
+      return serviceMap[_serviceId].serviceName;
+  }
+
+// contract avaiableServiceProvider is avaiableService{
+    
+//     struct ServiceProvider{
+//         uint servicerProviderId;
+//         string serviceProviderName;
+//         string serviceProviderPhoneNumber;
+//         string serviceProviderEmail;
+//         address serviceProviderAddress;
+//         uint rank;
+//         Service service;
+//     }
   
 }
 
-contract customer{
-    
-    struct Client{
-        uint clientId;
-        string clientName;
-        string clientPhoneNumber;
-        string clientEmail;
-    }
-}
