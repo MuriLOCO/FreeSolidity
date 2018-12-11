88f32955610f4ffb64e43806ed81fd15bc192785
zsnejom <jose.x.mendes@assistera.com>
2018-12-10 22:27:27 -0500

    Added funciontallity to add ranking to Service Providers

--- freeSolidity.sol
+++ freeSolidity.sol
@@ -24,6 +24,7 @@ contract FreeSolidityApplication{
         string serviceProviderEmail;
         address serviceProviderAddress;
         uint rank;
+        uint rankAvaliations;
     }
     
     uint serviceIdCounter;
@@ -65,7 +66,7 @@ contract FreeSolidityApplication{
        
        uint serviceProviderId = serviceProviderIdCounter++;
        serviceProviderMap[serviceProviderId] = ServiceProvider(serviceProviderId, _serviceProviderName, _serviceProviderPhoneNumber,
-       _serviceProviderEmail, msg.sender, 1);
+       _serviceProviderEmail, msg.sender, 5, 0);
    }
 
     //WIP: register as client (Sidd)
@@ -76,11 +77,29 @@ contract FreeSolidityApplication{
     }
 
     //TODO: match client with service provider (specify service type)
-    //TODO: add rank functionallty
+
+   //This function add rank to the service provider (Jose)
+    function rankServiceProvider(string memory _servicePoviderName, uint _rank) public{
+        uint currentRank = 0;
+        uint currentRankAvaliations = 0;
+        uint pointer = 0;
+        for(uint i = 1; i < serviceProviderIdCounter; i++){
+            if(compareStrings(serviceProviderMap[serviceProviderIdCounter].serviceProviderName, _servicePoviderName)){
+               currentRank = serviceProviderMap[serviceProviderIdCounter].rank;
+               currentRankAvaliations = serviceProviderMap[serviceProviderIdCounter].rankAvaliations;
+               pointer = i;
+               break;
+            }
+        }
+        currentRankAvaliations++;
+        currentRank = (currentRank + _rank) / currentRankAvaliations;
+        serviceProviderMap[pointer].rank = currentRank;
+        serviceProviderMap[pointer].rankAvaliations = currentRankAvaliations;
+    }
 
     //Jose
     //Returning lists in Solidity are still not supported and very expensive, so a solution found is to ask the user to
-    //choose a pointer and check if the service exist or not, in this case the user choose a string and we return true and false
+    //choose a pointer and check if the service exist or not, in this case the user choose a string and we return the service description
     function getServiceDescriptionOfAServiceByName(string memory _serviceName) public view returns (string memory){
         for(uint i = 1; i < serviceIdCounter; i++){
             if(compareStrings(serviceMap[i].serviceName, _serviceName)){
